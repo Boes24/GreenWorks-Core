5073eab985912175c0bd0fa6852ec3ff0735aced
Boes24 <cboes2402@gmail.com>
2025-08-12 07:50:49 +0200

    Add MowerState optiona and update version to beta

--- src/GreenWorksAPI/__init__.py
+++ src/GreenWorksAPI/__init__.py
@@ -1,4 +1,8 @@
 import logging
-
 # Ensure library logging integrates with host app (e.g., Home Assistant)
-logging.getLogger(__name__)
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry):
+    _LOGGER.info("Initializing GreenWorksAPI client")  # Now will show
+    return True
